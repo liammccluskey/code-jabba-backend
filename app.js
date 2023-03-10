8612ee1b487ed75b63bb467dfe46baf4726ec2b4
Liam Mccluskey <liammail100@gmail.com>
2023-03-10 10:21:03 -0500

    added cors options

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const mongoose = require('mongoose')
 require('dotenv/config')
 
 // Middleware
-app.use(cors())
+const corsOptions = { 
+    origin: '*', 
+    credentials: true,
+    optionSuccessStatus: 200,
+ }
+app.use(cors(corsOptions))
+
 app.use(express.json())
 
 app.use((req, res, next) => {
