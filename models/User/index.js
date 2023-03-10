d834560a803267800985e7c88790d3bdd90378a5
Liam Mccluskey <liammail100@gmail.com>
2023-03-10 17:19:41 -0500

    made displayname optional

--- models/User/index.js
+++ models/User/index.js
@@ -9,7 +9,7 @@ const UserSchema = mongoose.Schema({
     },
     displayName: {
         type: String,
-        required: true,
+        required: false,
     },
     email: {
         type: String,
