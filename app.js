6dcb2757db451bffed1ad28680c475f11c864afa
Liam Mccluskey <liammail100@gmail.com>
2023-03-10 10:27:24 -0500

    testing another cors option

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ const corsOptions = {
  }
 app.use(cors(corsOptions))
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+  });
+
 app.use(express.json())
 
 app.use((req, res, next) => {
