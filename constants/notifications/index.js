ccf97d0b576b5d63343d02adf7cbd76026083d23
Liam Mccluskey <liammail100@gmail.com>
2023-03-15 21:14:41 -0400

    minor change

--- constants/notifications/index.js
+++ constants/notifications/index.js
@@ -8,6 +8,6 @@ module.exports.NOTIFICATIONS = {
     // notificationID : { title, message, ?photoURL }
     welcomeToSite: {
         title: `Welcome to ${process.env.SITE_NAME}`,
-        message: ''
+        message: 'Welcome to the site.'
     }
 }
