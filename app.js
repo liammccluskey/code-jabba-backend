213ef7157b2c24acd0f77c4918f990f5f2b3f7c2
Liam Mccluskey <liammail100@gmail.com>
2024-11-11 18:25:05 -0500

    fixed dev env issue

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 require('dotenv/config')
 
 const {hasAdminPrivileges} = require('./src/routes/admin/utils')
+const {ENV} = require('./src/constants')
 
 // Middleware
 
@@ -12,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use((req, res, next) => {
-    console.log(req.originalUrl)
+    if (ENV === 'dev') console.log(req.originalUrl)
     next()
 })
 
