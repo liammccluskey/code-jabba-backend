485d2947be29f860634d032bf85f0efeb92fb874
Liam Mccluskey <liammail100@gmail.com>
2025-09-11 19:23:49 -0400

    fixed bug

--- src/models/Subscription/constants.js
+++ src/models/Subscription/constants.js
@@ -12,10 +12,22 @@ const SUBSCRIPTION_PRICE_IDS = {
         candidatePremium: 'price_1S2BbVJYyxSlwqx9XpFSdSi9',
     },
     PROD: {
-        recruiterPremium: '',
-        candidatePremium: '',
+        recruiterPremium: 'price_1S6Jj8Qsae153RTX1wUXg4wS',
+        candidatePremium: 'price_1S6JgLQsae153RTXTCTwixTb',
     }
 }[process.env.PROFILE_ENV]
+
+// const SUBSCRIPTION_PRICE_IDS = {
+//     DEV: {
+//         recruiterPremium: 'price_1S2BgEJYyxSlwqx9CyYPH0LB',
+//         candidatePremium: 'price_1S2BbVJYyxSlwqx9XpFSdSi9',
+//     },
+//     PROD: {
+//         recruiterPremium: 'price_1S6JXFQsae153RTX1VgkhhVS',
+//         candidatePremium: 'price_1S6JXKQsae153RTXr5DLZscj',
+//     }
+// }[process.env.PROFILE_ENV]
+
 
 module.exports = {
     SUBSCRIPTION_TIERS,
